60ae056a9e3154198349f862dab01a2da07d99b5
M3hdi <alpacinomerbah@gmail.com>
2020-12-16 15:48:40 +0000

    Update scripts.sh

--- WES_Workflow/scripts.sh
+++ WES_Workflow/scripts.sh
@@ -2,33 +2,33 @@
 
 
 ####################### FASTQ ############################
-awk -F ":" '$1=="@HWI-ST330" && $5==1' testfile.txt > filtered.txt
 
 #This Script can be used to filter out bad tiles manually, below is an example only, edit the range + ID 
 grep -P -A 4 '@HWI-D00119:50:H7AP8ADXX:1:(?!1[1-2][0-9][0-9])' sample.fastq
 
-
+#####################    SAM/BAM    ######################
 
 samtools flagstat 392_aln.bam 
 
 #################### Mean MAPQ #######################
-Average MAPQ for mapped reads: Filter out mapped reads and add up their quality then divide by the count.
+#Average MAPQ for mapped reads: Filter out mapped reads and add up their quality then divide by the count.
 
 samtools view -F 0x04 392_aln.bam | awk '{s+=$5} END {print "Avg Quality: ", s/NR}'
-Avg Quality:  25.5262
+#Avg Quality:  25.5262
+
 
 ################## No Indel Reads ####################
 
-Reads with no insertions or deletions:
+#Reads with no insertions or deletions:
 
 samtools view 392_aligned_sorted_RGcorr.bam | cut -f 6 | awk  '{if($0 !~ /[ID]/) sum+=1} END {print "No Ins or Del:", sum }' 
 
-No Ins or Del: 61300855
+#No Ins or Del: 61300855
 
 
 ######################### VCF/BCF ####################
 
-awk '$6>=90' headerless_file.vcf > ninety_filt.vcf
+
 
 
 
